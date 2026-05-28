086f9cc446db562f78470ca251db9e1aa6d74559
Gabriel Dias Redü <gabrielredu@gmail.com>
2026-05-28 18:19:46 -0300

    Changed PRODUCTS to instantiate in { }; Created method to add new products to the list; Added method to generate new id

--- src/main/java/br/com/gbrlrd/sb_api_curso_souzadev/service/ProductService.java
+++ src/main/java/br/com/gbrlrd/sb_api_curso_souzadev/service/ProductService.java
@@ -1,6 +1,7 @@
 package br.com.gbrlrd.sb_api_curso_souzadev.service;
 
 import br.com.gbrlrd.sb_api_curso_souzadev.database.model.ProductEntity;
+import br.com.gbrlrd.sb_api_curso_souzadev.dto.ProductDto;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -10,34 +11,63 @@ import java.util.List;
 @Service
 public class ProductService {
 
-	private static final List<ProductEntity> PRODUCTS = List.of(
-		ProductEntity.builder()
+	private static final List<ProductEntity> PRODUCTS = new ArrayList<>();
+
+	static {
+		PRODUCTS.add(
+			ProductEntity.builder()
 			.id(0)
 			.name("Lorem ipsum")
 			.price(new BigDecimal(10))
 			.quantity(1)
-			.build(),
-		ProductEntity.builder()
+			.build()
+		);
+		PRODUCTS.add(
+			ProductEntity.builder()
 			.id(1)
 			.name("Sit dolor")
 			.price(new BigDecimal(24513))
 			.quantity(2)
-			.build(),
-		ProductEntity.builder()
+			.build()
+		);
+		PRODUCTS.add(
+			ProductEntity.builder()
 			.id(2)
 			.name("Amet")
 			.price(new BigDecimal(67))
 			.quantity(7)
-			.build(),
-		ProductEntity.builder()
+			.build()
+		);
+		PRODUCTS.add(
+			ProductEntity.builder()
 			.id(3)
 			.name("Consectetur")
 			.price(new BigDecimal(987))
 			.quantity(3)
 			.build()
-	);
+		);
+	}
 
 	public List<ProductEntity> findAll() {
 		return new ArrayList<>(PRODUCTS);
+	}
+
+	public ProductEntity createProduct(ProductDto productDto) {
+
+		int newId = PRODUCTS.stream()
+				.mapToInt(ProductEntity::getId)
+				.max()
+				.orElse(0) + 1;
+
+		ProductEntity newProduct = ProductEntity.builder()
+				.id(newId)
+				.name(productDto.getName())
+				.price(productDto.getPrice())
+				.quantity(productDto.getQuantity())
+				.build();
+
+		PRODUCTS.add(newProduct);
+
+		return newProduct;
 	}
 }
